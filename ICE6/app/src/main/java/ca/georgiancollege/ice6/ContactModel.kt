b50f09a3cf4c0bcfeede4227daeac91a7213cda5
Sukhpreet Saini <119287107+SUKH2022@users.noreply.github.com>
2024-06-12 16:26:13 -0400

    added DataManager Singleton

--- ICE6/app/src/main/java/ca/georgiancollege/ice6/ContactModel.kt
+++ ICE6/app/src/main/java/ca/georgiancollege/ice6/ContactModel.kt
@@ -3,6 +3,14 @@ package ca.georgiancollege.ice6
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
+/**
+ * Represents a contact model.
+ *
+ * @param fullName The full name of the contact.
+ * @param contactNumber The contact number of the contact.
+ * @param emailAddress The email address of the contact.
+ */
+
 @JsonClass(generateAdapter = true)
 data class ContactModel(
     @Json(name= "FullName") val fullName: String,
@@ -10,6 +18,12 @@ data class ContactModel(
     @Json(name= "EmailAddress") val emailAddress: String
 )
 {
+    /**
+     * Returns a string representation of the contact model.
+     *
+     * @return A string representation of the contact model.
+     */
+
     override fun toString(): String {
         return """Full Name: $fullName, Contact Number: $contactNumber, Email Address: $emailAddress"""
     }
