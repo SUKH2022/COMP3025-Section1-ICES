9b6b7aac981dd29a285816445b1f654dbfcd4f8a
Sukhpreet Saini <119287107+SUKH2022@users.noreply.github.com>
2024-07-03 11:14:27 -0400

    added TvShowHolder

---
+++ ICE7/app/src/main/java/ca/georgiancollege/ice7/TvShowViewHolder.kt
@@ -0,0 +1,19 @@
+package ca.georgiancollege.ice7
+
+import androidx.recyclerview.widget.RecyclerView
+import ca.georgiancollege.ice7.databinding.TextRowItemBinding
+
+// TVShowViewHolder is a ViewHolder class that holds a reference to the TextRowItemBinding
+// and displays the TVShow data in the UI for each row
+class TVShowViewHolder (val binding: TextRowItemBinding):
+    RecyclerView.ViewHolder(binding.root)
+{
+    // This method binds the TVShow data to the UI elements in the ViewHolder
+    fun bind(tvShow: TVShow)
+    {
+        // Sets the text for the title, genre, and rating TextView
+        binding.title.text = tvShow.title
+        binding.genre.text = tvShow.genre
+        binding.rating.text = tvShow.rating.toString()
+    }
+}
