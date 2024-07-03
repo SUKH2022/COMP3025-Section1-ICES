6de00f2b0385363fb7bc60c97b74dfa1883a1fb6
Sukhpreet Saini <119287107+SUKH2022@users.noreply.github.com>
2024-07-03 10:00:47 -0400

    added TVShow Data Class

--- ICE7/app/src/main/java/ca/georgiancollege/ice7/MainActivity.kt
+++ ICE7/app/src/main/java/ca/georgiancollege/ice7/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ca.georgiancollege.ice7.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
     private lateinit var binding: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
