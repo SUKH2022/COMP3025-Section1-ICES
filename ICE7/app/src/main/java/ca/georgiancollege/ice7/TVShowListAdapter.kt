d8980eca3a8dfe5ae108c0aa325dd19027bfb7af
Sukhpreet Saini <119287107+SUKH2022@users.noreply.github.com>
2024-07-03 11:24:06 -0400

    added TVShowListAdapter

---
+++ ICE7/app/src/main/java/ca/georgiancollege/ice7/TVShowListAdapter.kt
@@ -0,0 +1,30 @@
+package ca.georgiancollege.ice7
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import ca.georgiancollege.ice7.databinding.TextRowItemBinding
+
+class TVShowListAdapter(private val onItemClicked: (TVShow) -> Unit):
+    ListAdapter<TVShow, TVShowViewHolder>(TVShowComparator())
+{
+    // Called when a new ViewHolder (Custom TableView Cell) is created
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TVShowViewHolder {
+        // Inflate the layout for each row
+        val binding = TextRowItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        // Return a new ViewHolder instance with the binding object
+        return TVShowViewHolder(binding)
+    }
+
+    // Called by the RecyclerView to display the data at a specific position
+    override fun onBindViewHolder(holder: TVShowViewHolder, position: Int) {
+        // Get the TVShow at the current position
+        val current = getItem(position)
+        // Bind the TVShow data to the ViewHolder's UI elements
+        holder.bind(current)
+        // Set a click listener for the row item to handle item clicks
+        holder.itemView.setOnClickListener {
+            onItemClicked(current)
+        }
+    }
+}
