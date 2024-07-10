0761c2dddbd8ee069022c4e99ceaa6d15d4ef024
Sukhpreet Saini <119287107+SUKH2022@users.noreply.github.com>
2024-07-10 09:44:57 -0400

    added dependencies for Firebase and Firestore

---
+++ ICE8/app/src/main/java/ca/georgiancollege/section1_ice8/AppDatabase.kt
@@ -0,0 +1,38 @@
+package ca.georgiancollege.section1_ice8
+
+import android.content.Context
+import androidx.room.*
+
+// This annotation marks the AppDatabase class as a Room database
+@Database(entities = [TVShow::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase()
+{
+    // This method returns the TVShowDao interface
+    abstract fun tvShowDao(): TVShowDao
+
+    companion object
+    {
+        // Volatile annotation ensures that the value of m_instance is always up-to-date
+        @Volatile
+        private var m_instance: AppDatabase? = null
+
+        // This function returns the singleton instance of the AppDatabase
+        // if the instance is null it creates a new instance using Room.databaseBuilder
+        fun getDatabase(context: Context): AppDatabase
+        {
+            // multiple  threads can ask for the database at the same time,
+            // so we use synchronized to ensure only one thread can access the database at a time
+            return m_instance ?: synchronized(this) {
+                // creates a new instance of the database if it doesn't exist
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "media_database" // name of the database file
+                ).build()
+                m_instance = instance
+
+                return instance
+            }
+        }
+    }
+}
---
+++ ICE8/app/src/main/java/ca/georgiancollege/section1_ice8/TVShowDao.kt
@@ -0,0 +1,28 @@
+package ca.georgiancollege.section1_ice8
+
+import androidx.room.*
+
+@Dao
+interface TVShowDao {
+    // Inserts a new TVShow into the database
+    // If the TVSHow with the same ID already exists, it will be replaced
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(tvShow: TVShow)
+
+    // Updates an existing TVShow in the database
+    @Update
+    suspend fun update(tvShow: TVShow)
+
+    // Deletes a TVShow from the database
+    @Delete
+    suspend fun delete(tvShow: TVShow)
+
+    // Retrieves all TVShows from the database
+    @Query("SELECT * FROM tv_shows")
+    suspend fun getAllTVShows(): List<TVShow>
+
+    // Retrieves a TVShow by its ID from the database
+    @Query("SELECT * FROM tv_shows WHERE id = :id")
+    suspend fun getTVShowById(id: Int): TVShow?
+
+}
