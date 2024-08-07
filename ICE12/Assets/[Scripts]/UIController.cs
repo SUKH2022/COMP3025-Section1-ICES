0975c35b34baeb8a54eb504364a0cd9043098145
Sukhpreet Saini <119287107+SUKH2022@users.noreply.github.com>
2024-08-07 01:44:24 -0400

    added Restart Button functionality

---
+++ ICE12/Assets/[Scripts]/UIController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    public void OnSceneButton_Pressed()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
